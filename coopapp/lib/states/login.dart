7a4f39ea4c43d0553348f6af1b793af5a8f034dd
palmycr <palmycr@abc.com>
2022-06-30 14:37:11 +0700

    update

--- coopapp/lib/states/login.dart
+++ coopapp/lib/states/login.dart
@@ -11,6 +11,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +21,73 @@ class _LoginState extends State<Login> {
           children: [
             buildImage(),
             buildAppName(),
+            buildUser(),
+            buildPassWord(),
           ],
         ),
       ),
+    );
+  }
+
+  Row buildUser() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 16),
+          width: 250,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3style(),
+              labelText: 'User :',
+              prefixIcon: const Icon(Icons.account_circle_outlined),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.dark)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.light)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassWord() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 16),
+          width: 250,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      statusRedEye = !statusRedEye;
+                    });
+                  },
+                  icon: statusRedEye
+                      ? Icon(
+                          Icons.remove_red_eye,
+                          color: MyConstant.dark,
+                        )
+                      : Icon(
+                          Icons.remove_red_eye_outlined,
+                          color: MyConstant.dark,
+                        )),
+              labelStyle: MyConstant().h3style(),
+              labelText: 'Password :',
+              prefixIcon: const Icon(Icons.lock_outline),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.dark)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.light)),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
