ebb966b02e414a9367d35ef9dc1cb2efc4867b57
palmycr <palmycr@abc.com>
2022-07-15 12:37:28 +0700

    Update api

---
+++ coopapp/api/insertUser.php
@@ -0,0 +1,42 @@
+<?php
+	include 'connected.php';
+	header("Access-Control-Allow-Origin: *");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    
+    exit;
+}
+
+if (!$link->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $link->error);
+    exit();
+	}
+
+if (isset($_GET)) {
+	if ($_GET['isAdd'] == 'true') {
+				
+		$user = $_GET['user'];	
+		$password = $_GET['password'];	
+		$name = $_GET['name'];
+		$lastname = $_GET['lastname'];
+		$ime = $_GET['ime'];
+		
+							
+		$sql = "INSERT INTO `Member`(`id`, `user`, `password`, `name`, `lastname`, `ime`) VALUES (Null,'user','password','name','lastname','ime')";
+
+		$result = mysqli_query($link, $sql);
+
+		if ($result) {
+			echo "true";
+		} else {
+			echo "false";
+		}
+
+	} else echo "Welcome Master UNG";
+   
+}
+	mysqli_close($link);
+?>
